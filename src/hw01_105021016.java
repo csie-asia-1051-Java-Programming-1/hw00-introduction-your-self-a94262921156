8948585008c97e5de2b1c6592671031b8ec4be5c
Shengjie0266 <105021040@live.asia.edu.tw>
2017-02-24 15:06:29 +0800

    homework1

---
+++ src/hw01_105021016.java
@@ -0,0 +1,13 @@
+
+public class hw01_105021016 {
+
+	public static void main(String[] args) {
+		//自我介紹
+		//於2/24
+		//作者:劉昌明
+		System.out.println("姓名:劉昌明");
+		System.out.println("性別:男");
+		System.out.println("興趣:跳舞");
+	}
+
+}
